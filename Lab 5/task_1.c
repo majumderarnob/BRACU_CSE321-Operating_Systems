0c75b908de7524e1a3eb40d0bf40ceb4f936328d
Arnob Majumder <arnobmajumder00@gmail.com>
2022-12-16 16:31:48 +0600

    Update task_1.c

--- Lab 5/task_1.c
+++ Lab 5/task_1.c
@@ -1,83 +1,63 @@
-#include <stdio.h>
 #include <pthread.h>
-#include <semaphore.h>
-#include <stdlib.h>
-
-#define MAX 10
+#include <stdio.h>
+#include <string.h>
+#define MAX 10 //producers and consumers can produce and consume upto MAX
 #define BUFLEN 6
-#define NUMTHREAD 2
-
-void *consumer(int *id);
-void *producer(int *id);
-
+#define NUMTHREAD 2 /* number of threads */
+void * consumer(int *id);
+void * producer(int *id);
 char buffer[BUFLEN];
-char source[BUFLEN];
+char source[BUFLEN]; //from this array producer will store it's production into buffer
 int pCount = 0;
 int cCount = 0;
 int buflen;
-
+//initializing pthread mutex and condition variables
 pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t nonEmpty = PTHREAD_COND_INITIALIZER;
 pthread_cond_t full = PTHREAD_COND_INITIALIZER;
-int thread_id[NUMTHREAD] = {0, 1};
+int thread_id[NUMTHREAD] = {0,1};
 int i = 0;
 int j = 0;
 
-main()
-{
-    int i;
+int main() {
+
     pthread_t thread[NUMTHREAD];
     strcpy(source, "abcdef");
     buflen = strlen(source);
-    for (i = 0; i < NUMTHREAD; i++)
-    {
-        pthread_create(&thread[i], NULL, producer, &thread_id[i]);
-    }
-    for (i = 0; i < NUMTHREAD; i++)
-    {
-        pthread_create(&thread[i], NULL, consumer, &thread_id[i]);
-    }
-    for (i = 0; i < NUMTHREAD; i++)
-    {
+    pthread_create(&thread[0], NULL, (void *)producer, &thread_id[0]);
+    pthread_create(&thread[1], NULL, (void *)consumer, &thread_id[1]);
+    for (int i=0; i<NUMTHREAD; i++){
         pthread_join(thread[i], NULL);
     }
-    printf("\n");
 }
 
-void *producer(int *id)
-{
-    int i;
-    pthread_mutex_lock(&count_mutex);
-    while (pCount == MAX)
-    {
-        printf("Producer %d is waiting for empty buffer\n", *id);
-        pthread_cond_wait(&nonEmpty, &count_mutex);
-    }
-    for (i = 0; i < BUFLEN; i++)
-    {
-        buffer[i] = source[i];
-    }
-    pCount++;
-    printf("%d is produced %s in buffer\n", *id, source);
-    pthread_cond_signal(&full);
-    pthread_mutex_unlock(&count_mutex);
-}
+void *producer(int *id) {
 
-void *consumer(int *id)
-{
-    int i;
-    pthread_mutex_lock(&count_mutex);
-    while (cCount == MAX)
-    {
-        printf("Consumer %d is waiting for full buffer\n", *id);
-        pthread_cond_wait(&full, &count_mutex);
+    while (i < MAX) {
+        pthread_mutex_lock(&count_mutex);
+        while ((pCount + 1) % BUFLEN == cCount) {
+            pthread_cond_wait(&nonEmpty, &count_mutex);
+        }
+        buffer[pCount] = source[pCount % buflen];
+        printf("%d produced %c by Thread %d\n", i, buffer[pCount], *id);
+        pCount = (pCount + 1) % BUFLEN;
+        i++;
+        pthread_cond_signal(&full);
+        pthread_mutex_unlock(&count_mutex);
+        sleep(1);
     }
-    for (i = 0; i < BUFLEN; i++)
-    {
-        buffer[i] = '\0';
+}
+void *consumer(int *id) {
+    while (j < MAX) {
+        pthread_mutex_lock(&count_mutex);
+        while (cCount == pCount) {
+            pthread_cond_wait(&full, &count_mutex);
+        }
+        printf("%d consumed %c by Thread  %d\n", j, buffer[cCount], *id);
+        cCount = (cCount + 1) % BUFLEN;
+        j++;
+        pthread_cond_signal(&nonEmpty);
+        pthread_mutex_unlock(&count_mutex);
+        sleep(4);
     }
-    cCount++;
-    printf("%d is consuming %s from buffer\n", *id, buffer);
-    pthread_cond_signal(&nonEmpty);
-    pthread_mutex_unlock(&count_mutex);
 }
